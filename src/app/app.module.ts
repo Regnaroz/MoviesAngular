a3a9bf6aea0a3a1e67aea0377d78a1933eff12eb
Regnaroz <k-0-1@outlook.com>
2021-10-18 17:51:49 +0300

    loginForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule  } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './Home/home-page/home-page.component';
@@ -42,7 +42,8 @@ import { TestComponent } from './customer/test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
